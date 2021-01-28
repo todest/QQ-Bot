0b73689cfaef7c90e19db53ec334310276ab7f2d
todest <1123792492@qq.com>
2021-01-28 11:30:51 +0800

    change wallpaper api

--- app/plugin/wallpaper.py
+++ app/plugin/wallpaper.py
@@ -4,13 +4,13 @@ from graia.application import MessageChain, Image
 
 class Test(Plugin):
 	cmd = '.wp'
-	brief_help = cmd + '\t动漫图片\r\n'
-	full_help = '动漫图片来源于新浪'
+	brief_help = cmd + '\t必应壁纸\r\n'
+	full_help = '壁纸来源于必应。'
 
 	def process(self):
 		try:
 			self.resp = MessageChain.create([
-				Image.fromNetworkAddress(r'http://api.btstu.cn/sjbz/?lx=dongman')
+				Image.fromNetworkAddress(r'https://bing.ioliu.cn/v1/rand?w=1920&h=1080')
 			])
 		except Exception as e:
 			print(e)
