48af7c0f473ab4d453f770e25104038619ecedb3
todest <1123792492@qq.com>
2021-03-08 16:29:14 +0800

    change news

--- app/plugin/readhub.py
+++ app/plugin/readhub.py
@@ -11,35 +11,30 @@ from app.plugin.base import Plugin
 from app.util.tools import line_break
 
 
+def _get_news() -> str:
+    api = "https://api.readhub.cn/topic?lastCursor=&pageSize=20"
+    req = requests.get(url=api)
+    if req.status_code != 200:
+        return "HTTP GET ERROR!"
+    news_digest = ""
+    resp_json = json.loads(req.text)
+    news_list = resp_json["data"]
+    for news in news_list:
+        news_digest += news["title"] + "\n"
+    return news_digest
+
+
 class ReadHub(Plugin):
     entry = ['.news', '.新闻']
     brief_help = '\r\n[√]\t新闻：news'
-    full_help = '.新闻/.news [num]\r\n获取num数量的科技新闻信息，默认数量为5。'
-
-    def _get_news(self) -> str:
-        if self.msg:
-            self.msg = self.msg[0]
-        else:
-            self.msg = '5'
-        self.msg = int(self.msg)
-        assert self.msg in range(1, 21)
-        api = "https://api.readhub.cn/topic?lastCursor=&pageSize=" + str(self.msg)
-        req = requests.get(url=api)
-        if req.status_code != 200:
-            return "HTTP GET ERROR!"
-        news_digest = ""
-        resp_json = json.loads(req.text)
-        news_list = resp_json["data"]
-        for news in news_list:
-            news_digest += news["title"] + "\n"
-        return news_digest
+    full_help = '.新闻/.news 获取科技新闻信息。'
 
     async def process(self):
         try:
             char_counts = 40
             font_size = 30
             padding = 60
-            news = self._get_news().split('\n')[:-1]
+            news = _get_news().split('\n')[:-1]
             for i in range(len(news)):
                 news[i] = f'{i + 1}. {news[i]}。'
 
@@ -56,22 +51,16 @@ class ReadHub(Plugin):
             dr.text((padding, padding), text=title, font=h1, fill='BLACK', spacing=4)
             dr.text((padding, padding * 2), text=news, font=font, fill='BLACK', spacing=4)
             im_bytes = BytesIO()
-            im.save(im_bytes)
+            im.save(im_bytes, format='png')
             self.resp = MessageChain.create([
                 Img.fromUnsafeBytes(im_bytes.getvalue())
             ])
-        except ValueError as e:
-            print(e)
-            self.arg_type_error()
-        except AssertionError as e:
-            print(e)
-            self.index_error()
         except Exception as e:
             print(e)
             self.unkown_error()
 
 
 if __name__ == '__main__':
-    a = ReadHub(MessageChain.create([Plain('.news 20')]))
+    a = ReadHub(MessageChain.create([Plain('.news')]))
     asyncio.run(a.get_resp())
     print(a.resp)
