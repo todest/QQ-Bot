1819bbed0f3704bb5d007cfbd21ea50fae89adfd
todest <1123792492@qq.com>
2021-04-09 12:25:27 +0800

    modify schedule group

--- app/extend/schedule.py
+++ app/extend/schedule.py
@@ -12,7 +12,7 @@ async def custom_schedule(loop, bcc, bot):
 
     @sche.schedule(crontabify('0 7 * * *'))
     async def every_day_dujitang():
-        target = await bot.getGroup(ID_TO_GROUP[4])
+        target = await bot.getGroup(ID_TO_GROUP[1])
         obj = DuJiTang(MessageChain.create([Plain('.jt')]), target)
         await obj.get_resp()
         await bot.sendGroupMessage(target, obj.resp)
